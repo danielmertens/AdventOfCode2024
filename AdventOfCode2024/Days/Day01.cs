ce553a67b9a5546aa9719ca88a682bd87f8fefa8
Daniel Mertens <danielmertensfaema@hotmail.com>
2024-12-01 06:56:25 +0100

    Day 1

--- AdventOfCode2024/Days/Day01.cs
+++ AdventOfCode2024/Days/Day01.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Diagnostics;
+
 namespace AdventOfCode2024.Days;
 
 internal class Day01 : AbstractDay
@@ -6,31 +7,113 @@ internal class Day01 : AbstractDay
     protected override void Execute()
     {
         UseExample = false;
-        long result = 0;
-
+        
         var lines = GetInputLines();
-        var parsedInfo = Parse(lines);
-        Calculate(parsedInfo);
+        
+        var stopwatch = Stopwatch.StartNew();
+        
+        var (left, right) = Parse(lines);
+        var (sum, multi) = Calculate(left, right);
 
-        Result(result);
+        stopwatch.Stop();
+
+        Console.WriteLine(stopwatch.Elapsed.ToString());
+
+        Result(sum);
+        Result(multi);
     }
 
-    private void Calculate(object parsedInfo)
+    private (int sum, int multi) Calculate(OrderedList left, OrderedListWithCounts right)
     {
+        var sum = 0;
+        var multi = 0;
 
-
-    }
-
-    private object Parse(string[] lines)
-    {
-
-        foreach (var line in lines)
+        for (var i = 0; i < left.Count; i++)
         {
-            // Do something...
-
-
+            sum += Math.Abs(left[i] - right[i]);
+            var count = right.Occurrence(left[i]);
+            multi += left[i] * count;
         }
 
-        return null;
+        return (sum, multi);
+    }
+
+    private (OrderedList left, OrderedListWithCounts right) Parse(string[] lines)
+    {
+        var left = new OrderedList();
+        var right = new OrderedListWithCounts();
+
+        for (var i = 0; i < lines.Length; i++)
+        {
+            var split = lines[i].Split("   ");
+            left.Add(ParseIntQuick(split[0]));
+            right.Add(ParseIntQuick(split[1]));
+        }
+
+        return (left, right);
+    }
+
+    private int ParseIntQuick(string text)
+    {
+        var number = 0;
+        for(var i = 0; i < text.Length; i++)
+        {
+            var numb = text[i] - '0';
+            number = (number * 10) + numb;
+        }
+
+        return number;
+    }
+
+    private class OrderedList : List<int>
+    {
+        public new virtual void Add(int item)
+        {
+            if (Count == 0)
+            {
+                base.Add(item);
+                return;
+            }
+
+            for (var i = 0; i < Count; i++)
+            {
+                if (item > this[i])
+                {
+                    Insert(i, item);
+                    return;
+                }
+            }
+
+            base.Add(item);
+        }
+    }
+
+    private class OrderedListWithCounts : OrderedList
+    {
+        public Dictionary<int, int> countlist = [];
+
+        public override void Add(int item)
+        {
+            base.Add(item);
+            
+            if (countlist.ContainsKey(item))
+            {
+                countlist[item]++;
+            }
+            else
+            {
+                countlist.Add(item, 1);
+            }
+        }
+
+        public int Occurrence(int item)
+        {
+            if (countlist.TryGetValue(item, out var count))
+            {
+                return count;
+            }
+
+            return 0;
+        }
     }
 }
